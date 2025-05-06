1efbe0d9364491aebf26e546c6222e5dd9b12f38
Banti Shaw <bantishaw@Bantis-Air.bbrouter>
2025-05-06 19:31:05 +0530

    Vue practice in small project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <div>
+    <HeaderBar />
+    <router-view/>
+    <FooterBar />
+  </div>
+</template>
+
+<script>
+import HeaderBar from './components/Header';
+import FooterBar from './components/Footer';
+
+export default {
+  name: 'App',
+  components : {HeaderBar, FooterBar}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/About.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="about">
+        <h1>{{msg}}</h1>
+        <p>
+            Reference site about Lorem Ipsum, giving information on 
+            its origins, as well as a random Reference site about Lorem Ipsum, giving information
+             on its origins, as well as a random
+            Reference site about Lorem Ipsum, giving information on its origins, as well as a random
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+      msg: 'Welcome to Your About Us Page'
+    }
+  }
+}
+</script>
+
+<style scoped>
+    .about {
+        padding:10px;
+        margin:10px;
+        background-color: #f3f3f3;
+        color:#384141;
+    }
+</style>
